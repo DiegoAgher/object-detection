bb1f679e4683f2016851610ef61e3775cebabdb7
DiegoAgher <diego.aguado@wizeline.com>
2017-12-21 11:23:35 -0600

    add checkpoints constant

--- training/constants.py
+++ training/constants.py
@@ -1,2 +1,3 @@
 DESCRIPTION_TRAIN_PARSER = 'Training script for training a RetinaNet' \
                            ' network following annotations on csv.'
+MODEL_CHECKPOINT_PATH = 'resnet50_csv_{}_{}.h5'
