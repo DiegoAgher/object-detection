41d47f8479a74ac18c4a86d9d09283bbe1aa8875
DiegoAgher <diego.aguado@wizeline.com>
2017-12-22 10:11:02 -0600

    add script to translate images that include wheels

--- data_augmentation/utils.py
+++ data_augmentation/utils.py
@@ -8,30 +8,41 @@ def get_random_translation(image_array, xmin, xmax, ymin, ymax):
     is_horizontal_translation = np.random.randint(0, 2, 1) > 0
 
     if is_horizontal_translation:
-        return get_random_horizontal_translation_matrix(image_array,
-                                                        xmin, xmax)
+        translation_matrix, xmin, xmax = \
+            random_horizontal_translation(image_array, xmin, xmax)
 
-    return get_random_vertical_translation_matrix(image_array, ymin, ymax)
+    else:
+        translation_matrix, ymin, ymax = \
+            random_vertical_translation(image_array, ymin, ymax)
+
+    return translation_matrix, xmin, xmax, ymin, ymax
 
 
-def get_random_horizontal_translation_matrix(image_array, xmin, xmax):
+def random_horizontal_translation(image_array, xmin, xmax):
     columns = image_array.shape[1]
     is_left_translation = np.random.randint(0, 2, 1) > 0
-    rand_translation = np.random.randint(0, columns - xmax, 1)
+    rand_translation = np.random.randint(0, columns - xmax + 1, 1)[0]
     if is_left_translation:
-        rand_translation = -1 * np.random.randint(0, xmin, 1)
-    return np.float32([[1, 0, rand_translation],
-                       [0, 1, 0]])
+        rand_translation = -1 * np.random.randint(0, xmin, 1)[0]
+    translation_matrix = np.float32([[1, 0, rand_translation], [0, 1, 0]])
+
+    xmin += rand_translation
+    xmax += rand_translation
+    return translation_matrix, int(xmin), int(xmax)
 
 
-def get_random_vertical_translation_matrix(image_array, ymin, ymax):
+def random_vertical_translation(image_array, ymin, ymax):
     rows = image_array.shape[0]
     is_up_translation = np.random.randint(0, 2, 1) > 0
-    rand_translation = np.random.randint(0, rows - ymax, 1)
+    rand_translation = np.random.randint(0, rows - ymax + 1, 1)[0]
     if is_up_translation:
-        rand_translation = -1 * np.random.randint(0, ymin, 1)
-    return np.float32([[1, 0, 0],
-                       [0, 1, rand_translation]])
+        rand_translation = -1 * np.random.randint(0, ymin, 1)[0]
+    translation_matrix = np.float32([[1, 0, 0], [0, 1, rand_translation]])
+
+    ymin += rand_translation
+    ymax += rand_translation
+
+    return translation_matrix, int(ymin), int(ymax)
 
 
 def get_bound_box(df_row):
--- data_augmentation/wheels_images_translation.py
+++ data_augmentation/wheels_images_translation.py
@@ -3,18 +3,41 @@ import cv2
 
 from data_augmentation.utils import get_bound_box, get_random_translation
 from utils.preprocessing.constants import ANNOTATIONS_COLUMNS_RETINA_FORMAT,\
-    FILE_LOCATION_COLUMN, OBJECT_COLUMN
+    FILE_LOCATION_COLUMN, OBJECT_COLUMN, WHEELS_COLUMN, NUMERIC_COLUMNS
 
 train = pd.read_csv('train_retina_format.csv', header=None,
                     names=ANNOTATIONS_COLUMNS_RETINA_FORMAT)
-train.fillna('', inplace=True)
 
-wheels_data = train[train[OBJECT_COLUMN] == 'wheels']
+wheels_data = train[train[OBJECT_COLUMN] == 'wheels'].copy()
 
-for _, row in wheels_data.iterrows():
+for row_index, row in wheels_data.iterrows():
+    if row_index % 100 == 0:
+        print("Augmenting row {}".format(row_index))
     image_path = row[FILE_LOCATION_COLUMN]
     image_array = cv2.imread(image_path)
-    rows, cols, _ = image_array.shape
+    rows, columns, _ = image_array.shape
     xmin, xmax, ymin, ymax = get_bound_box(row)
-    M_t = get_random_translation(image_array, xmin, xmax, ymin, ymax)
-    dst = cv2.warpAffine(image_array, M_t, (cols, rows))
+    translation_matrix, xmin, xmax, ymin, ymax = \
+        get_random_translation(image_array, xmin, xmax, ymin, ymax)
+
+    translated_image = cv2.warpAffine(image_array, translation_matrix,
+                                      (columns, rows))
+    translated_image_path = image_path.replace('.', '_t.')
+    cv2.imwrite(translated_image_path, translated_image)
+    new_image_row = pd.DataFrame(
+        [translated_image_path, xmin, ymin,
+         xmax, ymax, WHEELS_COLUMN]).transpose()
+    new_image_row.columns = ANNOTATIONS_COLUMNS_RETINA_FORMAT
+    train = train.append(new_image_row)
+
+
+for col in NUMERIC_COLUMNS:
+    train[col] = train[col].astype(str)
+
+train.to_csv('train_retina_format_augmented.csv', header=None, index=False)
+
+
+
+
+
+
