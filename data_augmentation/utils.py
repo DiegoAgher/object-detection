9876d974f06f2dee73de093ef3784647feb66c50
DiegoAgher <diego.aguado@wizeline.com>
2017-12-22 15:03:02 -0600

    add documentation of utilery for data augmentation

--- data_augmentation/utils.py
+++ data_augmentation/utils.py
@@ -1,3 +1,4 @@
+"""Utilery for data augmentation of images"""
 import numpy as np
 import cv2
 
@@ -6,6 +7,12 @@ from utils.preprocessing.constants import FILE_LOCATION_COLUMN, XMIN_COLUMN,\
 
 
 def get_image_and_bounding_box(image_metadata):
+    """
+    Uses image metadata to read the image and obtain bounding box
+    :param image_metadata: panda DataFrame row with corresponding attributes:
+            - file_location, xmin, xmax, ymin, ymax
+    :return: tuple with image_array and separate bounding box
+    """
     image_path = image_metadata[FILE_LOCATION_COLUMN]
     image_array = cv2.imread(image_path)
     xmin, xmax, ymin, ymax = get_bound_box(image_metadata)
@@ -13,6 +20,15 @@ def get_image_and_bounding_box(image_metadata):
 
 
 def randomly_translate_image(image_array, xmin, xmax, ymin, ymax):
+    """
+    Performs either a vertical or horizontal translation of an image
+    :param image_array: np.array usually coming the output of an imread method
+    :param xmin: int
+    :param xmax: int
+    :param ymin: int
+    :param ymax: int
+    :return: np.array representing the translated image
+    """
     translation_matrix, xmin, xmax, ymin, ymax = \
         get_random_translation(image_array, xmin, xmax, ymin, ymax)
     rows, columns, _ = image_array.shape
@@ -21,6 +37,15 @@ def randomly_translate_image(image_array, xmin, xmax, ymin, ymax):
 
 
 def get_random_translation(image_array, xmin, xmax, ymin, ymax):
+    """
+    Wrapper for random horizontal or vertical translation
+    :param image_array: np.array usually coming the output of an imread method
+    :param xmin: int
+    :param xmax: int
+    :param ymin: int
+    :param ymax: int
+    :return: np.array representing the translation, bounding box per coordinate
+    """
     is_horizontal_translation = np.random.randint(0, 2, 1) > 0
 
     if is_horizontal_translation:
@@ -35,6 +60,15 @@ def get_random_translation(image_array, xmin, xmax, ymin, ymax):
 
 
 def random_horizontal_translation(image_array, xmin, xmax):
+    """
+    Performs horizontal translation on an image, randomly to the left or right,
+    redefining the bounding box by the translation
+    :param image_array: np.array usually coming the output of an imread method
+    :param xmin: int
+    :param xmax: int
+    :return: np.array of the translation matrix, new xmin and new xmax for
+    bounding box
+    """
     columns = image_array.shape[1]
     is_left_translation = np.random.randint(0, 2, 1) > 0
     rand_translation = np.random.randint(0, columns - xmax + 1, 1)[0]
@@ -48,6 +82,15 @@ def random_horizontal_translation(image_array, xmin, xmax):
 
 
 def random_vertical_translation(image_array, ymin, ymax):
+    """
+    Performs vertical translation on an image, randomly up or down
+    redefining the bounding box by the translation
+    :param image_array: np.array usually coming the output of an imread method
+    :param ymin: int
+    :param ymax: int
+    :return: np.array of the translation matrix, new ymin and new ymax for
+    bounding box
+    """
     rows = image_array.shape[0]
     is_up_translation = np.random.randint(0, 2, 1) > 0
     rand_translation = np.random.randint(0, rows - ymax + 1, 1)[0]
@@ -62,5 +105,11 @@ def random_vertical_translation(image_array, ymin, ymax):
 
 
 def get_bound_box(df_row):
+    """
+    Wrapper to quickly get bounding box of per coordinate
+    :param df_row: pandas DataFrame row with corresponding attributes:
+            - xmin, xmax, ymin, ymax
+    :return: tuple of ints defining the bounding box
+    """
     return int(df_row[XMIN_COLUMN]), int(df_row[XMAX_COLUMN]),\
            int(df_row[YMIN_COLUMN]), int(df_row[YMAX_COLUMN])
