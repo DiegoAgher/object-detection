a19536df2854acf97024061a3ff2294a6442b28a
DiegoAgher <diego.aguado@wizeline.com>
2017-12-21 18:39:52 -0600

    remove unused variables, pylint enhancement

--- data_augmentation/wheels_images_translation.py
+++ data_augmentation/wheels_images_translation.py
@@ -1,14 +1,9 @@
-import numpy as np
 import pandas as pd
 import cv2
 
 from data_augmentation.utils import get_bound_box, get_random_translation
 from utils.preprocessing.constants import ANNOTATIONS_COLUMNS_RETINA_FORMAT,\
     FILE_LOCATION_COLUMN, OBJECT_COLUMN
-
-from utils.preprocessing.constants import XMIN_COLUMN, YMIN_COLUMN,\
-    XMAX_COLUMN, YMAX_COLUMN
-
 
 train = pd.read_csv('train_retina_format.csv', header=None,
                     names=ANNOTATIONS_COLUMNS_RETINA_FORMAT)
@@ -23,7 +18,3 @@ for _, row in wheels_data.iterrows():
     xmin, xmax, ymin, ymax = get_bound_box(row)
     M_t = get_random_translation(image_array, xmin, xmax, ymin, ymax)
     dst = cv2.warpAffine(image_array, M_t, (cols, rows))
-
-
-
-
